071c852635810c842c9123ac04545569e4afe67a
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-03-02 00:28:14 +0100

    Iteration #6

---
+++ 6.ex.js
@@ -0,0 +1,31 @@
+/* Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, 
+en caso que existan los elimina para retornar un array sin los elementos duplicados.
+ Puedes usar este array para probar tu función: */
+
+ const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+  function removeDuplicates(param) {
+    // insert code
+    const noDuplicates = [];
+    for( i = 0 ; i < param.length ; i++){
+        if(!noDuplicates.includes(param[i])){
+           noDuplicates.push(param[i]);
+        }
+    }
+    return noDuplicates;
+  }
+const resultado = removeDuplicates(duplicates);
+
+console.log(resultado);
+
