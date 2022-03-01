672af7c9d9689a107e5c13bdc840fe474359850a
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-03-01 13:44:55 +0100

    Iteration #1

---
+++ 1.ex.js
@@ -0,0 +1,15 @@
+/* Completa la función que tomando dos números como argumento devuelva el más alto. */
+
+function sum(numberOne , numberTwo) {
+    // insert code
+    if (numberOne > numberTwo){
+        return numberOne
+    }else{
+        return numberTwo
+    }
+}
+
+let  Resultado = sum(5, 25)
+
+console.log(' el numero mas alto es:'+ Resultado)
+
